acb532be1cc8a24450302ba7f72a5f9986b56f69
MorshedMushfiq <dangerboymorshed90@yahoo.com>
2023-10-05 15:36:32 +0600

    updated for storage path solved and success file upload

---
+++ resources/views/success.blade.php
@@ -0,0 +1,17 @@
+<x-frontheader />
+
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio perspiciatis nisi maiores aspernatur odit quaerat? Aut esse quisquam dolores voluptatum! Hic, impedit? Vero.</p>
+
+    <h1 class='text-center bg-dark text-light p-3 my-2'>{{$msg}}</h1>
+
+
+
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure debitis, eum laborum possimus corrupti obcaecati ea repellat. Ullam a eius, aliquid aspernatur obcaecati, earum rem quos labore velit voluptate illum!</p>
+
+    <a class='btn btn-success' href="{{route('cake.index')}}">Back to home</a>
+
+
+
+<x-frontfooter />
